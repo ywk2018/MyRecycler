0e32583de3203eea511d1de43143995943f959a2
ywk <987378653@qq.com>
2019-05-31 23:43:06 +0800

    瀑布流实现

--- app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.myrecyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,25 +18,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initFruit();
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
         FruitAdapter fruitAdapter = new FruitAdapter(mFruits);
         recyclerView.setAdapter(fruitAdapter);
-
     }
 
     private void initFruit() {
         for (int i = 0; i < 10; i++) {
-            MyFruit apple = new MyFruit("apple", R.mipmap.apple);
+            MyFruit apple = new MyFruit(getRandom("apple"), R.mipmap.apple);
             mFruits.add(apple);
-            MyFruit banana = new MyFruit("banana", R.mipmap.banana);
+            MyFruit banana = new MyFruit(getRandom("banana"), R.mipmap.banana);
             mFruits.add(banana);
-            MyFruit orange = new MyFruit("orange", R.mipmap.oriange);
+            MyFruit orange = new MyFruit(getRandom("orange"), R.mipmap.oriange);
             mFruits.add(orange);
-            MyFruit cherry = new MyFruit("cherry", R.mipmap.cherry);
+            MyFruit cherry = new MyFruit(getRandom("cherry"), R.mipmap.cherry);
             mFruits.add(cherry);
-            MyFruit melon = new MyFruit("memon", R.mipmap.melon);
+            MyFruit melon = new MyFruit(getRandom("memon"), R.mipmap.melon);
             mFruits.add(melon);
         }
+    }
+
+    private String getRandom(String name){
+
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        int length = random.nextInt(20) + 1;
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(name);
+        }
+        return stringBuilder.toString();
     }
 }
